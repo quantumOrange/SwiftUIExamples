8bf26a5a0b53067ff3286f85d6fedef94cdeccf9
David Crooks <crodave@gmail.com>
2021-03-02 10:18:12 +0100

    Initial Commit

---
+++ SwiftUIDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoApp
+//
+//  Created by David Crooks on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAppApp.swift
+//  SwiftUIDemoApp
+//
+//  Created by David Crooks on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
